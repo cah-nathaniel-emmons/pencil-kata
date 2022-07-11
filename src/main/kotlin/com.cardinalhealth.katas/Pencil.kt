19caf42e94172154fee40c63f2989aa5d38bc4ce
Nate Emmons <nathaniel.emmons@cardinalhealth.com>
2022-07-11 15:14:10 -0400

    Write on the paper with a pencil

--- src/main/kotlin/com.cardinalhealth.katas/Pencil.kt
+++ src/main/kotlin/com.cardinalhealth.katas/Pencil.kt
@@ -1,5 +1,15 @@
 package com.cardinalhealth.katas
 
-class Paper(private val contents: String = "") {
+class Paper(private var contents: String = "") {
     fun read() = contents
+
+    fun write(characters: String) {
+        contents = characters
+    }
+}
+
+class Pencil() {
+    fun write(paper: Paper, characters: String) {
+        paper.write(characters)
+    }
 }
--- src/test/kotlin/PencilTest.kt
+++ src/test/kotlin/PencilTest.kt
@@ -1,4 +1,5 @@
 import com.cardinalhealth.katas.Paper
+import com.cardinalhealth.katas.Pencil
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
@@ -14,5 +15,13 @@ class PencilTest {
     fun `we can read a piece of paper that has text on it`() {
         val paper = Paper("Hello World")
         assertEquals("Hello World", paper.read())
+    }
+
+    @Test
+    fun `we can write on the paper with a pencil`() {
+        val pencil = Pencil()
+        val paper = Paper()
+        pencil.write(paper, "Hello!")
+        assertEquals("Hello!", paper.read())
     }
 }
