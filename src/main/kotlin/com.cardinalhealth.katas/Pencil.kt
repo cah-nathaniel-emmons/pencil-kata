3c463363cae6d11512a38c0e4cb21438cb300141
Nate Emmons <nathaniel.emmons@cardinalhealth.com>
2022-07-11 15:08:12 -0400

    Read a blank piece of paper

--- src/main/kotlin/com.cardinalhealth.katas/Pencil.kt
+++ src/main/kotlin/com.cardinalhealth.katas/Pencil.kt
@@ -1,2 +1,6 @@
 package com.cardinalhealth.katas
 
+class Paper() {
+    private val contents: String = ""
+    fun read() = contents
+}
--- src/test/kotlin/PencilTest.kt
+++ src/test/kotlin/PencilTest.kt
@@ -1,3 +1,12 @@
+import com.cardinalhealth.katas.Paper
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
 class PencilTest {
 
+    @Test
+    fun `we can read a blank piece of paper`() {
+        val paper = Paper()
+        assertEquals("", paper.read())
+    }
 }
